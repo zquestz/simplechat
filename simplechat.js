748f0febac3e157450d4e1d4e8491972cdd731d3
quest <quest@mac.com>
2012-07-26 23:59:06 -0700

    Very basic chat app, no users yet

--- simplechat.js
+++ simplechat.js
@@ -1,19 +1,51 @@
+Messages = new Meteor.Collection("messages");
+
 if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to simplechat.";
+  Template.chat.messages = function () {
+    var messages = Messages.find({text: { $exists: true }, date: { $exists: true}, formatted_date: { $exists: true} }, { sort: {date: 1} });
+    var handle = messages.observe({
+      added: function (message) {
+        var new_size = 25 * Messages.find().count();
+        $('#chat').animate({ scrollTop: new_size }, 1);
+      }
+    });
+    return messages;
   };
 
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.chat.events = {
+    'submit form': function (event) {
+      var inputbox = $('#input').first(),
+          new_message = inputbox.val(),
+          date = new Date(),
+          hours = date.getHours(),
+          minutes = date.getMinutes(),
+          suffix = "AM";
+
+      if (hours >= 12) {
+        suffix = "PM";
+        hours = hours - 12;
+      }
+      
+      if (hours == 0) {
+        hours = 12;
+      }
+
+      if (minutes < 10) {
+        minutes = "0" + minutes
+      }
+
+      var formatted_date = hours + ":" + minutes + " " + suffix;
+
+      Messages.insert({text: new_message, date: date, formatted_date: formatted_date});
+      
+      inputbox.val('');
+
+      event.preventDefault();
+      event.stopPropagation();
     }
   };
 }
 
 if (Meteor.is_server) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  Meteor.startup(function () {});
 }
