ffef15f3b402e5f15b83c8b10dfd881b746f18d8
quest <quest@mac.com>
2012-07-28 00:38:22 -0700

    Added basic user support

--- simplechat.js
+++ simplechat.js
@@ -1,8 +1,9 @@
 Messages = new Meteor.Collection("messages");
+Users = new Meteor.Collection("users");
 
 if (Meteor.is_client) {
   Template.chat.messages = function () {
-    var messages = Messages.find({text: { $exists: true }, date: { $exists: true}, formatted_date: { $exists: true} }, { sort: {date: 1} });
+    var messages = Messages.find({user: { $exists: true }, text: { $exists: true }, date: { $exists: true}, formatted_date: { $exists: true} }, { sort: {date: 1} });
     var handle = messages.observe({
       added: function (message) {
         var new_size = 25 * Messages.find().count();
@@ -12,9 +13,46 @@ if (Meteor.is_client) {
     return messages;
   };
 
+  Template.chat.users = function () {
+    var users = Users.find({name: { $exists: true }}, { sort: {name: 1} });
+    return users;
+  };
+
+  Template.register.signed_in = Template.chat.signed_in = function () {
+    var logged_in = (Session.get("user") ? true : false);
+    return logged_in;
+  };
+
+  Template.register.warning = function () {
+    return Session.get('warning');
+  };
+
+  Template.register.events = {
+    'submit form': function (event) {
+      var registerbox = $('#register'),
+          username = registerbox.val(),
+          now = (new Date()).getTime();
+
+      if (username === "") {
+        Session.set('warning', 'Please enter a valid username.');
+      } else if (Users.findOne({name: username})) {
+        Session.set('warning', 'Username is already taken. Please choose another.');
+      } else if (username.length > 9) {
+        Session.set('warning', 'Username is too long. Please choose a username under 9 characters.');
+      } else {
+        Session.set('warning', null);
+        Session.set("user", username);
+        Users.insert({name: username, last_seen: now});
+      }      
+      
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  };
+
   Template.chat.events = {
     'submit form': function (event) {
-      var inputbox = $('#input').first(),
+      var inputbox = $('#input'),
           new_message = inputbox.val(),
           date = new Date(),
           hours = date.getHours(),
@@ -36,7 +74,7 @@ if (Meteor.is_client) {
 
       var formatted_date = hours + ":" + minutes + " " + suffix;
 
-      Messages.insert({text: new_message, date: date, formatted_date: formatted_date});
+      Messages.insert({user: Session.get("user"), text: new_message, date: date, formatted_date: formatted_date});
       
       inputbox.val('');
 
@@ -44,8 +82,33 @@ if (Meteor.is_client) {
       event.stopPropagation();
     }
   };
+
+  Meteor.setInterval(function () {
+    var username = Session.get('user');
+    Meteor.call('keepalive', username);
+  }, 5000);
 }
 
 if (Meteor.is_server) {
   Meteor.startup(function () {});
+
+  Meteor.setInterval(function () {
+    var now = (new Date()).getTime();
+
+    Users.find({last_seen: {$lt: (now - 60 * 1000)}}).forEach(function (user) {
+      Users.remove(user._id)
+    });
+  });
+
+  Meteor.methods({
+    keepalive: function (user) {
+      var now = (new Date()).getTime();
+
+      if (!Users.findOne({name: user})) {
+        Users.insert({name: user, last_seen: now});
+      }
+
+      Users.update({name: user}, {$set: {last_seen: now}});
+    }
+  });
 }
